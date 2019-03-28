69d4e689a5fc39ae3bb988430e0d0df1112bd9d2
Suguru Imanaga <suguru_imanaga@cysista.co.jp>
2019-03-29 04:58:48 +0900

    modify the action of outputing the reports

--- src/test/java/sample/integration/test/AppIT.java
+++ src/test/java/sample/integration/test/AppIT.java
@@ -1,11 +1,13 @@
 package sample.integration.test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppIT {
 
 	@Test
 	public void test() {
-		System.out.println("test");
+		assertEquals(System.getProperty("TEST"), "test");
 	}
 }
